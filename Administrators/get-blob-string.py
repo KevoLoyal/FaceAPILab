4c6241cd9df2b331540c38dcced8d1d1ecf16352
Kevin Leal <jordi.222@hotmail.com>
2019-11-17 17:32:51 -0600

    Update get-blob-string.py

--- Administrators/get-blob-string.py
+++ Administrators/get-blob-string.py
@@ -17,11 +17,11 @@ from os import path  # Import OS Path Function
 
 # Info to Blob
 
-blob_storage_key = 'MPcaH5oNp/+oNgexNGjSafKd4yl5Cxrnv/RlSEX+wNt4PYmCiT7CtHJ6at/5veg0705m70oS4xS35M6B/Y8rdA=='
+blob_storage_key = 'KEY STRING'
 
-connect_str = 'DefaultEndpointsProtocol=https;AccountName=cognitiveservicesblob;AccountKey=MPcaH5oNp/+oNgexNGjSafKd4yl5Cxrnv/RlSEX+wNt4PYmCiT7CtHJ6at/5veg0705m70oS4xS35M6B/Y8rdA==;EndpointSuffix=core.windows.net'
+connect_str = 'STRING '
 
-storage_account = 'cognitiveservicesblob'
+storage_account = 'Blob Name '
 
 container_name = "container1"
 
